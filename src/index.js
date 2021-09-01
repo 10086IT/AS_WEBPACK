5419f507a972649ebcd10ba3db0f49bc9b9bd15f
useStore <1409670346@qq.com>
2021-09-01 17:57:53 +0800

    as:webpack-dev-server   express启动服务来创建localhost:3000端口，需要手动刷新浏览器

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function component() {
     const element = document.createElement('div');
 
     // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-    element.innerHTML = "__**手动erf阀手dfvg动阀__"
+    element.innerHTML = "__**手动阀===手动阀__"
     element.classList.add('he')
     element.onclick=print;
     return element;
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,6 @@ module.exports = {
     },
     devServer: {
         static: './dist',
-        hot:true,
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -20,6 +19,7 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true
+        clean: true,
+        publicPath: '/',
     },
 };
