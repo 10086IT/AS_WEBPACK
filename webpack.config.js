a87acd4bc2aada6a077916356bd7409475ead14e
useStore <1409670346@qq.com>
2021-09-01 14:52:53 +0800

    as：打包字体文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const CSSTest = /\.css$/i;
 const ImageTest = /\.(png|svg|jpg|jpeg|gif)$/i ;
+const fontTest = /\.(woff|woff2|eot|ttf|otf)$/i;
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -15,6 +16,10 @@ module.exports = {
             },
             {
                 test:ImageTest,
+                type: 'asset/resource'
+            },
+            {
+                test:fontTest,
                 type: 'asset/resource'
             }
         ]
