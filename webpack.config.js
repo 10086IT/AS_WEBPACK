a5e0b02d16264c0d1fea03236bc33b301f135193
useStore <1409670346@qq.com>
2021-09-01 14:22:52 +0800

    as：添加css-loader、style-loader将css打包成style添加到header中

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
-
+const CSSTest = /\.css$/i;
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: CSSTest,
+                use: ['style-loader','css-loader']
+            }
+        ]
+    }
 };
