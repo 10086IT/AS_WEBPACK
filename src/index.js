145bd59c4653b260722f2513d6a7cdf4b3966b76
useStore <1409670346@qq.com>
2021-09-01 16:24:25 +0800

    webpack 生成 print.bundle.js 和 index.bundle.js 文件

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
-
+import print from './print.js'
 function component() {
     const element = document.createElement('div');
 
     // lodash（目前通过一个 script 引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], '*_*');
     element.classList.add('he')
+    element.onclick=print;
     return element;
 }
+print();
 document.body.appendChild(component());
