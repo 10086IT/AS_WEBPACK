f77e0603fcdf4c05907b4f5c9e354d4a78a48f14
useStore <1409670346@qq.com>
2021-09-01 15:46:51 +0800

    版本回退

--- src/index.js
+++ src/index.js
@@ -1,33 +1,11 @@
 import _ from 'lodash';
-import './css.css'
-import ImageA from './a.PNG';
-import Data from './data.xml';
-import Notes from './data.csv';
-import json from './json.json'
-import toml from './toml.toml';
-import yaml from './yaml.yaml';
-import json5 from './json5.json5';
 
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json5.title); // output `JSON5 Example`
-console.log(json5); // output `Tom Preston-Werner`
 function component() {
     const element = document.createElement('div');
 
     // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack', json.data], '*_*');
+    element.innerHTML = _.join(['Hello', 'webpack'], '*_*');
     element.classList.add('he')
-    const myImg = new Image(400, 400);
-    myImg.src = ImageA;
-    element.appendChild(myImg);
     return element;
 }
-
-console.log(Notes);
-console.log(Data);
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,9 @@
 const path = require('path');
-const CSSTest = /\.css$/i;
-const ImageTest = /\.(png|svg|jpg|jpeg|gif)$/i;
-const fontTest = /\.(woff|woff2|eot|ttf|otf)$/i;
-const XmlTest = /\.xml$/i;
-const CsvAndTsvTest = /\.(csv|tsv)$/i;
-const TomlTest =/\.toml$/i;
-const YamlTest =/\.yaml$/i;
-const Json5Test = /\.json5$/i;
-const toml = require('toml');
-const yaml = require('yamljs');
-const json5 = require('json5');
+
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    module: {
-        rules: [
-            {
-                test: CSSTest,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: ImageTest,
-                type: 'asset/resource'
-            },
-            {
-                test: fontTest,
-                type: 'asset/resource'
-            },
-            {
-                test: CsvAndTsvTest,
-                use: ['csv-loader'],
-            },
-            {
-                test: XmlTest,
-                use: ['xml-loader'],
-            },
-            {
-                test:TomlTest ,
-                type: 'json',
-                parser: {
-                    parse: toml.parse,
-                },
-            },
-            {
-                test: YamlTest,
-                type: 'json',
-                parser: {
-                    parse: yaml.parse,
-                },
-            },
-            {
-                test:Json5Test ,
-                type: 'json',
-                parser: {
-                    parse: json5.parse,
-                },
-            },
-        ]
-    }
 };
