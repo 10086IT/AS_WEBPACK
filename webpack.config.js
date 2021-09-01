7210bb2fb3e98bd031f56aa8e6faaab2047ae606
useStore <1409670346@qq.com>
2021-09-01 16:57:11 +0800

    '定位错误位置'

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,17 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode:'development',
     entry: {
         index: './src/index.js',
         print: './src/print.js',
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: '管理输出',
+            title: 'development',
         }),
     ],
+    devtool: 'inline-source-map',
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
